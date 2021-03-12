8f1c7e38993b1a3fe1a76befaf07384dde2bf51c
ahnty0122 <ahnty0122@naver.com>
2021-03-12 18:02:05 +0900

    하노이탑

---
+++ hanoi.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// 직접 재귀로 구현
+void rHanoi(int n, char from, char aux, char to)
+{
+    if(n == 1)
+        printf("Disk %d : Move from %c to %c\n", n, from, to);
+    else
+    {
+        rHanoi(n - 1, from, to, aux); // n-1개 원판을 가운데 aux로 놓기
+        printf("Disk %d : Move from %c to %c\n", n, from, to);
+        rHanoi(n - 1, aux, from, to);
+    }
+}
+
+void main()
+{
+    rHanoi(4, 'A', 'B', 'C');
+}

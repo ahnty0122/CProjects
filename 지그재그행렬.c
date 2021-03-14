dc9b6df292bbbd2528ad37ca4308f44689bdb792
ahnty0122 <ahnty0122@naver.com>
2021-03-14 17:27:52 +0900

    지그재그, 나선형 행렬

--- 지그재그행렬.c
+++ 지그재그행렬.c
@@ -1,35 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SIZE 5
+#define ROW 4
+#define COLUMN 5
 
-// 홀수행은 오름차순, 짝수행은 내림차순
-void zigzag(int A[][SIZE])
+// 나선형 행렬 만들기
+void spiral(int A[][COLUMN])
 {
+    int left = 0;
+    int right = COLUMN - 1;
+    int top = 0;
+    int bottom = ROW - 1;
     int value = 1;
-    for(int i = 0; i < SIZE; i++)
+    while((left <= right) && (top <= bottom))
     {
-        if(i % 2 == 0)
-            for(int j = 0; j < SIZE; j++)
+        for(int j = left; j <= right; j++)
+        {
+            A[top][j] = value;
+            value++;
+        }
+        top++;
+        if(top <= bottom)
+        {
+            for(int i = top; i <= bottom; i++)
             {
-                A[i][j] = value;
+                A[i][right] = value;
                 value++;
             }
-        else
-            for(int j = SIZE - 1; j >= 0; j--)
+        }
+        right--;
+        if(left <= right)
+        {
+            for(int j = left; j <= right; j++)
             {
-                A[i][j] = value;
+                A[bottom][right - j + left] = value;
                 value++;
             }
+        }
+        bottom--;
+        if(top <= bottom)
+        {
+            for(int i = top; i <= bottom; i++)
+            {
+                A[bottom - i + top][left] = value;
+                value++;
+            }
+        }
+        left++;
     }
 }
 
-// 행렬 출력 함수
-void printArray(int A[][SIZE])
+void printArray(int A[][COLUMN])
 {
-    for(int i = 0; i < SIZE; i++)
+    for(int i = 0; i < ROW; i++)
     {
-        for(int j = 0; j < SIZE; j++)
+        for(int j = 0; j < COLUMN; j++)
             printf("%2d ", A[i][j]);
         printf("\n");
     }
@@ -37,10 +62,7 @@ void printArray(int A[][SIZE])
 
 void main()
 {
-    // 0으로 다 초기화
-    int A[SIZE][SIZE] = {0};
-    // 지그재그 행렬 생성
-    zigzag(A);
-    // 행렬 출력
+    int A[ROW][COLUMN] = {0}; 
+    spiral(A);
     printArray(A);
 }
